429637e580c1e3b53d9cd806b4ad90b51fa92ba0
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2018-06-16 00:53:02 +0100

    Prepare configurations

---
+++ IDE-Shell/Config/IConfigSection.cs
@@ -0,0 +1,15 @@
+﻿//  
+// Copyright (c) NickAc. All rights reserved.
+// Licensed under the LGPL v3 License. See LICENSE file in the project root for full license information.
+//  
+
+using System.ComponentModel.Composition;
+
+namespace NickAc.IDE_Shell.Config
+{
+    [InheritedExport(typeof(IConfigSection))]
+    public interface IConfigSection
+    {
+        string DisplayName { get; }
+    }
+}
